58a5646b3c836209ff4ccd7b98a8a348d7bc8a1f
RotaMatei <mateirotariu@gmail.com>
2025-10-28 18:46:22 +0200

    Added the some of the attributes for Card in Card.h and will start on implementing getters and setters for those in Card.cpp next

--- Models/Models/Card.cpp
+++ Models/Models/Card.cpp
@@ -1,1 +1,5 @@
 #include "Card.h"
+using namespace Models
+{
+
+}
--- Models/Models/Card.h
+++ Models/Models/Card.h
@@ -1,14 +1,32 @@
 #pragma once
+#include <array>
 #include <cstdint>
+#include <map>
+#include "Resources.h"
 
 namespace Models
 {
 	class Card
 	{
 	private:
-		enum class Resources : uint8_t{
+		std::map<ResourceType, uint8_t> m_resourceCost;
+		std::map<ResourceType, uint8_t> m_resourceProduction;
+		uint8_t m_victoryPoints : 1;
+		uint8_t m_shieldPoints : 1;
+		std::array<uint8_t, 7> m_scientificSymbols = { 0, 0, 0, 0, 0, 0, 0 }; 
 
-		};
+	public:
+		const std::map<ResourceType, uint8_t>& GetResourceCost() const;
+		const std::map<ResourceType, uint8_t>& GetResourceProduction() const;
+		uint8_t GetVictoryPoints() const;
+		uint8_t GetShieldPoints() const;
+		const std::array<uint8_t, 7>& GetScientificSymbols() const;
+
+		void SetResourceCost(const std::map<ResourceType, uint8_t>& resourceCost);
+		void SetResourceProduction(const std::map<ResourceType, uint8_t>& resourceProduction);
+		void SetVictoryPoints(uint8_t victoryPoints);
+		void SetShieldPoints(uint8_t shieldPoints);
+		void SetScientificSymbols(const std::array<uint8_t, 7>& scientificSymbols);
 	};
 }
 
