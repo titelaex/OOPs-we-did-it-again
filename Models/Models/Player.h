14cd768f3a96174d017ef4662a036a562d381f9a
Denisa <denisatrotus9@gmail.com>
2025-11-01 00:07:19 +0200

    Updated the Player.h file by declaring setters and getters for each attribute

--- Models/Models/Player.h
+++ Models/Models/Player.h
@@ -32,8 +32,45 @@ private:
 	int wonderVictoryPoints = 0;
 	int progressVictoryPoints = 0;
 
+public:
 
+	//setters
+	void setPlayerId(const std::string& id);
+	void setPlayerUsername(const std::string& username);
+	void setPassword(const std::string& pwd);
 
+	void setPositionOnBoard(int position);
+
+	void addBuilding(const Building& building);
+	void addWonder(const Wonder& wonder);
+	void addCard(const Card& card);
+
+	void setHasConflictPawn(bool hasPawn);
+	void setRemainingCoins(int coins);
+
+	void setMilitaryVictoryPoints(int points);
+	void setBuildingVictoryPoints(int points);
+	void setWonderVictoryPoints(int points);
+	void setProgressVictoryPoints(int points);
+
+	//getters
+	const std::string& getPlayerId() const;
+	const std::string& getPlayerUsername() const;
+	const std::string& getPassword() const;
+
+	int getPositionOnBoard() const;
+	const std::vector<Building>& getOwnedBuildings() const;
+	const std::vector<Wonder>& getOwnedWonders() const;
+	const std::vector<Card>& getOwnedCards() const;
+
+	bool getHasConflictPawn() const;
+
+	int getRemainingCoins() const;
+
+	int getMilitaryVictoryPoints() const;
+	int getBuildingVictoryPoints() const;
+	int getWonderVictoryPoints() const;
+	int getProgressVictoryPoints() const;
 };
 }
 
